059eb58f99cdff3869f4e62588dc6ff05e4c4bf2
Bek Johansson <beksaylor@gmail.com>
2025-04-16 15:08:09 -0500

    first commit

---
+++ src/test/java/edu/bekthedev/spring6WebApp/Spring6WebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.bekthedev.spring6WebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6WebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
